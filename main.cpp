eb0c046a484d633231a7d1c2910aa59ec018ee22
Zhe Deng <42720670+thezhe@users.noreply.github.com>
2021-12-28 21:36:35 -0500

    error test

--- main.cpp
+++ main.cpp
@@ -10,18 +10,30 @@ bool sameWav (string path0, string path1)
     file1.load(path1);
     
     if (file0.getSampleRate() != file1.getSampleRate())
+    {
+        cout << "SameWav: SampleRate mismatch\n";
         return false;
+    }
     if (file0.getNumSamplesPerChannel() != file1.getNumSamplesPerChannel())
+    {
+        cout << "SameWav: NumSamplesPerChannel mismatch\n";
         return false;
+    }
     if (file0.getNumChannels() != file1.getNumChannels())
+    {
+        cout << "SameWav: NumChannels mismatch\n";
         return false;
+    }
 
     for (int i = 0; i < file0.getNumChannels(); ++i)
     {
         for (int j = 0; j < file0.getNumSamplesPerChannel(); ++j)
         {
             if (file0.samples[i][j] != file1.samples[i][j])
+            {
+                cout << "SameWav: Sample mismatch\n";
                 return false;
+            }
         }
     }    
 
@@ -29,10 +41,8 @@ bool sameWav (string path0, string path1)
 }   
 
 int main (int argc, char* argv[]) {
-    cout << "Hello, World!" << endl;
-
     if (argc != 3)
-        cout << "Usage: ./SameWave.exe <wav file 0> <wav file 1>";
+        cout << "Usage: ./SameWave.exe <wav file 0> <wav file 1>\n";
     
     if (!sameWav(argv[1], argv[2]))
         return 1;
